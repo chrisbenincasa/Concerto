4a248d65af6799cbed7443c909a7801a04ecb6d8
Christian Benincasa <chrisbenincasa@gmail.com>
2014-09-21 09:54:32 -0400

    Initial Commit

---
+++ Concerto-Swift/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  Concerto-Swift
+//
+//  Created by Christian Benincasa on 9/21/14.
+//  Copyright (c) 2014 Christian Benincasa. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
---
+++ Concerto-SwiftTests/Concerto_SwiftTests.swift
@@ -0,0 +1,36 @@
+//
+//  Concerto_SwiftTests.swift
+//  Concerto-SwiftTests
+//
+//  Created by Christian Benincasa on 9/21/14.
+//  Copyright (c) 2014 Christian Benincasa. All rights reserved.
+//
+
+import Cocoa
+import XCTest
+
+class Concerto_SwiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
